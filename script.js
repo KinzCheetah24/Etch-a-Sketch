06aa4502419dda33021c57f99b2ed06205a00801
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-06-02 20:56:19 +0200

    Grid created
    The grid of 16x16 squared divs were created with JS, with styles.css file for styling

---
+++ script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+
+// The container were the grid is going to be
+const grid = document.createElement("div");
+grid.className = "container";
+body.appendChild(grid);
+
+const columns = 16;
+const rows = 16;
+
+// Create a grid of 16x16 square divs
+for (let i = 0 ; i < columns ; i++) {
+    let rowsDiv = document.createElement("div");
+    rowsDiv.className = "rowDiv";
+    grid.appendChild(rowsDiv);
+    for (let j = 0 ; j < rows ; j++) {
+        let columnsDiv = document.createElement("div");
+        columnsDiv.className = "gridDiv";
+        rowsDiv.appendChild(columnsDiv);
+    }
+}
