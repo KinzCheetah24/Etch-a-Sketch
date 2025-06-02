09428b6fe876053bafaed91e4065f3a7a2d88f55
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-06-02 21:55:18 +0200

    Introduce number of squares feature added
    The user can introduce the number of squares per side it wants, and  the logic for that and the styling were also added

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 const body = document.querySelector("body");
 
+const btn = document.querySelector("button");
+
 // The container were the grid is going to be
 const grid = document.createElement("div");
 grid.className = "container";
 body.appendChild(grid);
 
-const columns = 16;
-const rows = 16;
+let numberSquaresPerSide = 16;
 
-// Create a grid of 16x16 square divs
-for (let i = 0 ; i < columns ; i++) {
-    let rowsDiv = document.createElement("div");
-    rowsDiv.className = "rowDiv";
-    grid.appendChild(rowsDiv);
-    for (let j = 0 ; j < rows ; j++) {
-        let columnsDiv = document.createElement("div");
-        columnsDiv.className = "gridDiv";
-        rowsDiv.appendChild(columnsDiv);
-        columnsDiv.addEventListener("mousemove", () => {columnsDiv.style.backgroundColor = "blue"});
+createGrid(numberSquaresPerSide);
+
+btn.addEventListener("click", () => {
+    do {
+        numberSquaresPerSide = window.prompt("Enter a number (1 - 100)");
+    } while (numberSquaresPerSide > 100);
+
+    console.log(grid.childElementCount);
+
+    for (let i = 0, numberOfChilds = grid.childElementCount ; i < numberOfChilds ; i++) {
+        grid.lastChild.remove();
+    }
+
+    createGrid(numberSquaresPerSide);
+});
+
+function createGrid(squaresPerSide) {
+    for (let i = 0 ; i < squaresPerSide ; i++) {
+        let rowsDiv = document.createElement("div");
+        rowsDiv.className = "rowDiv";
+        for (let j = 0 ; j < squaresPerSide ; j++) {
+            let columnsDiv = document.createElement("div");
+            columnsDiv.className = "gridDiv";
+            rowsDiv.appendChild(columnsDiv);
+            columnsDiv.addEventListener("mousemove", () => {columnsDiv.style.backgroundColor = "blue"});
+        }
+        grid.appendChild(rowsDiv);
     }
 }
